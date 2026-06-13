6b8612910be6ce2de73c62ac0cac064f6d97e3c5
Suraj-Kabir <surajkabir.allenhouse@gmail.com>
2026-06-13 13:49:31 +0530

    GSM SET

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import Navbar from "@/components/component/Navbar";
 import Footer from "@/components/component/Footer";
 import { Providers } from "./providers";
 import "quill/dist/quill.snow.css";
+import Script from "next/script";
 
 
 
 export const metadata: Metadata = {
   title: "Synmac",
   description: "Chemicle Website",
+ 
 };
 
 async function getIndustries() {
@@ -136,7 +138,38 @@ export default async function RootLayout({ children }: any) {
 
   return (
     <html lang="en">
+
+       <head>
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MLS7S5VK');
+          `}
+        </Script>
+      </head>
+
+
       <body>
+
+     <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MLS7S5VK"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+
+
        
         <Providers>
            <Navbar getStories={getStories} getNews={getNews} getBlogs={getBlogs} getCompanyData={getCompanyData} productCategory={getAllProuductCategory}  subIndustory={getAllSubIndustory} data={industries} product={getProducts} allResources={allResources} />
